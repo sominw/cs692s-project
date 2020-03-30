a170d07cd20eac0514d6f48b59867039599e40f9
Somin Wadhwa <sominwadhwa@gmail.com>
2020-03-30 01:21:26 -0400

    relu_tvm

--- tvm_op.py
+++ tvm_op.py
@@ -8,7 +8,6 @@ def element_wise_addition(shape, func_name, dtype="float32", tgt="llvm", tgt_hos
     A = tvm.te.placeholder(shape, dtype=dtype, name="A")
     B = tvm.te.placeholder(shape, dtype=dtype, name="B")
     C = tvm.te.compute(A.shape, lambda *i: A(*i) + B(*i))
-
     s = tvm.te.create_schedule(C.op)
     f = tvm.build(s, [A, B, C], tgt, target_host=tgt_host, name=func_name)
     return f
@@ -17,7 +16,6 @@ def element_wise_addition_by_const(shape, const_k, func_name, dtype="float32", t
     A = tvm.te.placeholder(shape, dtype=dtype, name="A")
     B = tvm.tir.const(const_k, A.dtype)
     C = tvm.te.compute(A.shape, lambda *i: A(*i) + B)
-
     s = tvm.te.create_schedule(C.op)
     f = tvm.build(s, [A, C], tgt, target_host=tgt_host, name=func_name)
     return f
@@ -26,7 +24,31 @@ def element_wise_mul(shape, func_name, dtype="float32", tgt="llvm", tgt_host="ll
     A = tvm.te.placeholder(shape, dtype=dtype, name="A")
     B = tvm.te.placeholder(shape, dtype=dtype, name="B")
     C = tvm.te.compute(A.shape, lambda *i: A(*i) * B(*i))
-
     s = tvm.te.create_schedule(C.op)
     f = tvm.build(s, [A, B, C], tgt, target_host=tgt_host, name=func_name)
+    return f
+
+def element_wise_mul_by_const(shape, const_k, func_name, dtype="float32", tgt="llvm", tgt_host="llvm"):
+    A = tvm.te.placeholder(shape, dtype=dtype, name="A")
+    B = tvm.tir.const(const_k, A.dtype)
+    C = tvm.te.compute(A.shape, lambda *i: A(*i) * B)
+    s = tvm.te.create_schedule(C.op)
+    f = tvm.build(s, [A, C], tgt, target_host=tgt_host, name=func_name)
+    return f
+
+def relu(shape, func_name, dtype="float32", tgt="llvm", tgt_host="llvm"):
+    A = tvm.te.placeholder(shape, dtype=dtype, name="A")
+    B = tvm.tir.const(0, A.dtype)
+    C = tvm.te.compute(A.shape, lambda *i: tvm.tir.max(A(*i), B))
+    s = tvm.te.create_schedule(C.op)
+    f = tvm.build(s, [A, C], tgt, target_host=tgt_host, name=func_name)
+    return f
+
+def relu_grad(shape, func_name, dtype="float32", tgt="llvm", tgt_host="llvm"):
+    A = tvm.te.placeholder(shape, dtype=dtype, name="A")
+    B = tvm.te.placeholder(shape, dtype=dtype, name="B")
+    C = tvm.tir.const(0, A.dtype)
+    D = tvm.te.compute(A.shape, lambda *i: tvm.tir.expr.Select((A(*i) > C), B(*i), C))
+    s = tvm.te.create_schedule(D.op)
+    f = tvm.build(s, [A, B, D], tgt, target_host=tgt_host, name=func_name)
     return f
